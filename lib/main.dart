926aed0549fe43f6d4a2fb0140761abab7231d86
RenanYMartins <yohanssen.martins@gmail.com>
2024-08-15 11:36:53 -0300

    :sparkles: firtst commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+
+  // ignore: unused_field
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Despesas Pessoais')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              elevation: 5,
+              color: Colors.blue,
+              child: Text('Gráfico'),
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                        child: Text(
+                      tr.value.toString(),
+                    ),
+                    ),
+                    Column(
+                      children: [
+                        Text(tr.title),
+                        Text(tr.date.toString()),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
